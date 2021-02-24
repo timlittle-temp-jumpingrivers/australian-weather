6ca42b232c552bb9ca1a891030813846ac2ad261
Ooton <tsl13@ic.ac.uk>
2021-02-24 15:04:04 +0000

    Added tiem series

--- R/create-plot.R
+++ R/create-plot.R
@@ -29,5 +29,10 @@ city = "Sydney"
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 
-#Testing 'git pull'
-#Testing 'git merge'
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = scatter_plot)
